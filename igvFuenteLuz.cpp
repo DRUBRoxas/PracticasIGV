f5b113c35e24ad2120c9c548cc636af90b2a5d4c
Manuel Sánchez Salazar <mss00048@red.ujaen.es>
2025-12-13 13:43:24 +0100

    Añadidas luces y Configuraciones

---
+++ igvFuenteLuz.cpp
@@ -0,0 +1,5 @@
+//
+// Created by msanchez on 12/12/25.
+//
+
+#include "igvFuenteLuz.h"
---
+++ igvFuenteLuz.h
@@ -0,0 +1,14 @@
+//
+// Created by msanchez on 12/12/25.
+//
+
+#ifndef PR1A_IGVFUENTELUZ_H
+#define PR1A_IGVFUENTELUZ_H
+
+
+class igvFuenteLuz
+{
+};
+
+
+#endif //PR1A_IGVFUENTELUZ_H
